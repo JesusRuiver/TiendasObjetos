5d552e0b373309dfdcccc1953c76bc8a75145b2a
Jesús <jesusruiver@jesusruiver.com>
2018-05-27 20:45:08 +0200

    Exportar XML

--- src/ventanas/EjercicioExportarImportar.java
+++ src/ventanas/EjercicioExportarImportar.java
@@ -16,6 +16,23 @@ import javax.swing.JFrame;
 import javax.swing.JOptionPane;
 import javax.swing.JPanel;
 import javax.swing.border.EmptyBorder;
+import javax.xml.parsers.DocumentBuilder;
+import javax.xml.parsers.DocumentBuilderFactory;
+import javax.xml.parsers.ParserConfigurationException;
+import javax.xml.transform.Result;
+import javax.xml.transform.Source;
+import javax.xml.transform.Transformer;
+import javax.xml.transform.TransformerConfigurationException;
+import javax.xml.transform.TransformerException;
+import javax.xml.transform.TransformerFactory;
+import javax.xml.transform.TransformerFactoryConfigurationError;
+import javax.xml.transform.dom.DOMSource;
+import javax.xml.transform.stream.StreamResult;
+
+import org.w3c.dom.DOMImplementation;
+import org.w3c.dom.Document;
+import org.w3c.dom.Element;
+import org.w3c.dom.Text;
 
 import bbdd.Conexion;
 import clases.Articulo;
@@ -38,6 +55,9 @@ public class EjercicioExportarImportar extends JFrame {
 	private ArrayList<Pedido> pedidos;
 
 	private Conexion miConexion = new Conexion();
+
+	// private Document document;
+	// private Element raiz;
 
 	/**
 	 * Launch the application.
@@ -217,26 +237,28 @@ public class EjercicioExportarImportar extends JFrame {
 
 							Articulo articulo1 = new Articulo();
 
-							/*pedido1.setNif(pedidos.get(i).getNif());
-							pedido1.setNombreArticulo(pedidos.get(i).getNombreArticulo());
+							/*
+							 * pedido1.setNif(pedidos.get(i).getNif());
+							 * pedido1.setNombreArticulo(pedidos.get(i).getNombreArticulo());
+							 * 
+							 * fabricante1.setNombre(pedidos.get(i).getFabricante().getNombre());
+							 * pedido1.setFabricante(fabricante1);
+							 * 
+							 * pedido1.setPeso(pedidos.get(i).getPeso());
+							 * pedido1.setCategoria(pedidos.get(i).getCategoria());
+							 * pedido1.setFechaPedido(pedidos.get(i).getFechaPedido());
+							 * pedido1.setUnidadesPedidas(pedidos.get(i).getUnidadesPedidas());
+							 * 
+							 * articulo1.setPrecioVenta(pedidos.get(i).getArticulo().getPrecioVenta());
+							 * pedido1.setArticulo(articulo1);
+							 */
 
-							fabricante1.setNombre(pedidos.get(i).getFabricante().getNombre());
-							pedido1.setFabricante(fabricante1);
-
-							pedido1.setPeso(pedidos.get(i).getPeso());
-							pedido1.setCategoria(pedidos.get(i).getCategoria());
-							pedido1.setFechaPedido(pedidos.get(i).getFechaPedido());
-							pedido1.setUnidadesPedidas(pedidos.get(i).getUnidadesPedidas());
-
-							articulo1.setPrecioVenta(pedidos.get(i).getArticulo().getPrecioVenta());
-							pedido1.setArticulo(articulo1);*/
-							
 							pedido1.setNif(pedidos.get(i).getNif());
 							pedido1.setNombreArticulo(pedidos.get(i).getNombreArticulo());
 							pedido1.setCodFabricante(pedidos.get(i).getCodFabricante());
 							pedido1.setPeso(pedidos.get(i).getPeso());
 							pedido1.setCategoria(pedidos.get(i).getCategoria());
-							pedido1.setFechaPedido (pedidos.get(i).getFechaPedido());
+							pedido1.setFechaPedido(pedidos.get(i).getFechaPedido());
 							pedido1.setUnidadesPedidas(pedidos.get(i).getUnidadesPedidas());
 
 							dataOS.writeObject(pedido1); // inserta nombre
@@ -272,9 +294,10 @@ public class EjercicioExportarImportar extends JFrame {
 							Venta venta1;
 
 							venta1 = (Venta) dataIS.readObject();
-							
-							miConexion.insertaVenta(venta1.getNif(), venta1.getNombreArticulo(), venta1.getCodFabricante(),
-									venta1.getPeso(), venta1.getCategoria(), venta1.getFechaVenta(),venta1.getUnidadesVendidas());
+
+							miConexion.insertaVenta(venta1.getNif(), venta1.getNombreArticulo(),
+									venta1.getCodFabricante(), venta1.getPeso(), venta1.getCategoria(),
+									venta1.getFechaVenta(), venta1.getUnidadesVendidas());
 
 							System.out.println(venta1.getNif() + " " + venta1.getNombreArticulo());
 						}
@@ -298,11 +321,12 @@ public class EjercicioExportarImportar extends JFrame {
 						while (true) {
 
 							Pedido pedido1;
-							
+
 							pedido1 = (Pedido) dataIS.readObject();
-							
-							miConexion.insertaPedido(pedido1.getNif(), pedido1.getNombreArticulo(), pedido1.getCodFabricante(),
-									pedido1.getPeso(), pedido1.getCategoria(), pedido1.getFechaPedido(),pedido1.getUnidadesPedidas());
+
+							miConexion.insertaPedido(pedido1.getNif(), pedido1.getNombreArticulo(),
+									pedido1.getCodFabricante(), pedido1.getPeso(), pedido1.getCategoria(),
+									pedido1.getFechaPedido(), pedido1.getUnidadesPedidas());
 
 							System.out.println(pedido1.getNif() + " " + pedido1.getNombreArticulo());
 						}
@@ -320,6 +344,119 @@ public class EjercicioExportarImportar extends JFrame {
 		btnExportarXML.addActionListener(new ActionListener() {
 
 			public void actionPerformed(ActionEvent e) {
+
+				if (rbtnVentas.isSelected() == true) {
+
+					DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+
+					DocumentBuilder builder;
+					try {
+						builder = factory.newDocumentBuilder();
+
+						DOMImplementation implementation = builder.getDOMImplementation();
+						Document document = implementation.createDocument(null, "Ventas", null);
+						document.setXmlVersion("1.0");
+
+						for (int i = 0; i < ventas.size(); i++) {
+
+							Element raiz = document.createElement("venta"); // nodo venta
+							document.getDocumentElement().appendChild(raiz);
+
+							// añadir Nif
+							crearElemento("nif", ventas.get(i).getNif(), raiz, document);
+							// añadir nombreArticulo
+							crearElemento("nombreArticulo", ventas.get(i).getNombreArticulo(), raiz, document);
+							// añadir codFabricante
+							crearElemento("codFabricante", ventas.get(i).getCodFabricante() + "", raiz, document);
+							// añadir peso
+							crearElemento("peso", ventas.get(i).getPeso() + "", raiz, document);
+							// añadir categoria
+							crearElemento("categoria", ventas.get(i).getCategoria() + "", raiz, document);
+							// añadir fechaVenta
+							crearElemento("fechaVenta", ventas.get(i).getFechaVenta(), raiz, document);
+							// añadir unidadesVendidas
+							crearElemento("unidadesVendidas", ventas.get(i).getUnidadesVendidas() + "", raiz,
+									document);
+						}
+
+						Source source = new DOMSource(document);
+						Result result = new StreamResult(new java.io.File("Ventas.xml"));
+						Transformer transformer = TransformerFactory.newInstance().newTransformer();
+						transformer.transform(source, result);
+
+						JOptionPane.showMessageDialog(null, "Exportado Fichero Ventas.xml");
+
+					} catch (ParserConfigurationException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					} catch (TransformerConfigurationException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					} catch (TransformerFactoryConfigurationError e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					} catch (TransformerException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+
+				} else {
+					
+					DocumentBuilderFactory factory = DocumentBuilderFactory.newInstance();
+
+					DocumentBuilder builder;
+					try {
+						builder = factory.newDocumentBuilder();
+
+						DOMImplementation implementation = builder.getDOMImplementation();
+						Document document = implementation.createDocument(null, "Pedidos", null);
+						document.setXmlVersion("1.0");
+
+						for (int i = 0; i < pedidos.size(); i++) {
+
+							Element raiz = document.createElement("pedido"); // nodo venta
+							document.getDocumentElement().appendChild(raiz);
+
+							// añadir Nif
+							crearElemento("nif", pedidos.get(i).getNif(), raiz, document);
+							// añadir nombreArticulo
+							crearElemento("nombreArticulo", pedidos.get(i).getNombreArticulo(), raiz, document);
+							// añadir codFabricante
+							crearElemento("codFabricante", pedidos.get(i).getCodFabricante() + "", raiz, document);
+							// añadir peso
+							crearElemento("peso", pedidos.get(i).getPeso() + "", raiz, document);
+							// añadir categoria
+							crearElemento("categoria", pedidos.get(i).getCategoria() + "", raiz, document);
+							// añadir fechaPedido
+							crearElemento("fechaPedido", pedidos.get(i).getFechaPedido(), raiz, document);
+							// añadir unidadesPedidas
+							crearElemento("unidadesPedidas", pedidos.get(i).getUnidadesPedidas() + "", raiz,
+									document);
+						}
+
+						Source source = new DOMSource(document);
+						Result result = new StreamResult(new java.io.File("Pedidos.xml"));
+						Transformer transformer = TransformerFactory.newInstance().newTransformer();
+						transformer.transform(source, result);
+
+						JOptionPane.showMessageDialog(null, "Exportado Fichero Pedidos.xml");
+
+					} catch (ParserConfigurationException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					} catch (TransformerConfigurationException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					} catch (TransformerFactoryConfigurationError e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					} catch (TransformerException e1) {
+						// TODO Auto-generated catch block
+						e1.printStackTrace();
+					}
+
+
+				}
 
 			}
 
@@ -351,5 +488,16 @@ public class EjercicioExportarImportar extends JFrame {
 		System.out.println(nif);
 
 		return nif;
+	}
+
+	public void crearElemento(String dato, String valor, Element raiz, Document document) {
+
+		Element elem = document.createElement(dato);
+
+		Text text = document.createTextNode(valor); // damos valor
+
+		raiz.appendChild(elem); // pegamos el elemento hijo a la raiz
+
+		elem.appendChild(text); // pegamos el valor
 	}
 }
